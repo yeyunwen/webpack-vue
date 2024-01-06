7ba30c1cc456f973201054ee4076a8a578406486
yeyunwen <1571374338@qq.com>
2024-01-06 18:24:54 +0800

    feat: lazy-component暂存

---
+++ src/components/Big.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="big-container">Im' Big Component</div>
+</template>
+
+<script>
+export default {
+  name: "BigComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+.big-container {
+  width: 100%;
+  height: 300px;
+  background: gold;
+}
+</style>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,9 +1,58 @@
 <template>
-  <div class="test">test component</div>
+  <div class="test">
+    <h1>test component</h1>
+    <div class="zhanwei">1211</div>
+    <VueLazyComponent>
+      <Big />
+    </VueLazyComponent>
+    <div ref="lazyContainer" class="lazy-conatiner">
+      <div v-if="showContent" class="lazy-conatiner-content">content</div>
+    </div>
+  </div>
 </template>
 
-<style>
+<script>
+export default {
+  name: "TestComponent",
+};
+</script>
+
+<script setup>
+import Big from "./Big.vue";
+import { component as VueLazyComponent } from "@xunlei/vue-lazy-component";
+import { onMounted, ref } from "vue";
+
+const lazyContainer = ref(null);
+const showContent = ref(false);
+
+onMounted(() => {
+  const io = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        console.log("isIntersecting");
+      }
+    },
+    {
+      threshold: 0.5,
+      root: null,
+    }
+  );
+  io.observe(lazyContainer.value);
+});
+</script>
+
+<style lang="scss">
 .test {
   font-size: 20px;
+}
+.zhanwei {
+  height: 1000px;
+}
+.lazy-conatiner {
+  background: paleturquoise;
+  height: 300px;
+  .lazy-conatiner-content {
+    background: pink;
+  }
 }
 </style>
